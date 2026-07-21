c78df1c9963e6db81e1f4e6abb6cc52f184b5a40
Hermes <hermes@nousresearch.com>
2026-07-21 11:08:06 +0000

    feat: ajoute routes/hello.py - blueprint endpoint /hello

--- relance2/app/routes/hello.py
+++ relance2/app/routes/hello.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template
 
-hello_bp = Blueprint('hello', __name__, template_folder='../../templates')
+hello_bp = Blueprint('hello', __name__)
 
-@hello_bp.route('/hello', methods=['GET'])
+
+@hello_bp.route('/hello')
 def hello():
     return render_template('hello/index.html')
