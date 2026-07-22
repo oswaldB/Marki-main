d63f62f1d08c0297ba56fae29507ac6cd5de2c01
Hermes <hermes@nousresearch.com>
2026-07-22 16:23:32 +0000

    feat(login): implémentation cell

--- relance2/app/screens/login/routes/index.py
+++
@@ -1,9 +0,0 @@
-"""Main login page route."""
-from flask import render_template
-from .. import bp
-
-
-@bp.route('/', methods=['GET'])
-def index():
-    """Display login page."""
-    return render_template('login.html')
